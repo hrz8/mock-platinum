df04688059849ef4503ebefb7c7051f497f73544
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2022-08-01 19:52:30 +0700

    add server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 2001
+
+app.listen(PORT, () => console.log(`server listening on port: ${PORT}`));
