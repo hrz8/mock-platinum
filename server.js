545743051fe8d1d3b8ac18dd47e8411323dc1be1
G. Ferry <grgsferry@gmail.com>
2022-08-01 20:16:17 +0700

    add two more routes, profile and contact

--- server.js
+++ server.js
@@ -12,5 +12,7 @@ app.get("/", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("Hello from Hello");
 });
+app.get("/profile", (req, res) => {});
+app.get("/contact", (req, res) => {});
 
 app.listen(PORT, () => console.log(`server listening on port: ${PORT}`));
